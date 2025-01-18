388929d05599a4df63246130bd9522c22e33962e
duvan smith <dsmith.mr@gmail.com>
2025-01-18 11:24:03 -0500

    Eliminacion recarga

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,4 +1,6 @@
-export const NavBar = () => {
+import Link from "next/link";
+
+export const NavBar = async () => {
     console.log("Navbar creado");
     return (
         <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
@@ -8,9 +10,9 @@ export const NavBar = () => {
 
             <div className="flex flex-1"></div>
 
-            <a className="mr-2" href="/about">About</a>
-            <a className="mr-2" href="/contact">Contact</a>
-            <a className="mr-2" href="/pricing">Pricing</a>
+            <Link className="mr-2" href="/about">About</Link>
+            <Link className="mr-2" href="/contact">Contact</Link>
+            <Link className="mr-2" href="/pricing">Pricing</Link>
         </nav>
     )
 }
