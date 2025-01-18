f0b9114f6e60283baa7a9087cf3f072b36a075c1
duvan smith <dsmith.mr@gmail.com>
2025-01-18 11:18:30 -0500

    NavBar

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,12 +1,17 @@
+import { NavBar } from "@/components";
+
 export default function GeneralLayout({
     children
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <main className='flex flex-col items-center p-24'>
-            <span className='text-lg'>Hola mundo</span>
-            {children}
-        </main>
+        <>
+            <NavBar />
+            <main className='flex flex-col items-center p-24'>
+                <span className='text-lg'>Hola mundo</span>
+                {children}
+            </main>
+        </>
     );
 }
